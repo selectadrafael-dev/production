b2578378260743bb7e3105b09bdde3f90591e11c
doubleprince-code <adesojiade1@gmail.com>
2026-02-18 09:04:18 -0800

    Custom theme added v6

---
+++ addons/theme_gifting/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "Gifting Theme",
+    "description": "Custom theme for corporate gifting platform",
+    "version": "1.0",
+    "author": "SelectAd",
+    "category": "Theme/Corporate",
+    "depends": [
+            "website", 
+            "product", 
+            "sale",
+            "website_sale",
+            "sale_management",
+            "payment",
+            "website_sale_wishlist", 
+    ],
+    "data": [
+        #"views/assets.xml",
+        "views/header.xml",
+        "views/hero.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "theme_gifting/static/src/css/header.css",
+            "theme_gifting/static/src/css/hero.css",
+        ],
+    },
+    "theme": True,
+    "installable": True,
+    "application": False,
+}
