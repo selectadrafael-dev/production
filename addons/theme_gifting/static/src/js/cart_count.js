a703e3b9c06ceb2858371012ab8b1cbd395bd57f
doubleprince-code <adesojiade1@gmail.com>
2026-02-19 05:54:07 -0800

    Theme header updated v2

--- addons/theme_gifting/static/src/js/cart_count.js
+++ addons/theme_gifting/static/src/js/cart_count.js
@@ -9,6 +9,32 @@ odoo.define('theme_gifting.cart_count', function (require) {
         start: function () {
             fetch('/shop/cart/update_json', {
                 method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({})
+            })
+            .then(res => res.json())
+            .then(data => {
+                const qty = data.cart_quantity || 0;
+                this.el.querySelector('.cart-count').textContent = qty;
+            });
+
+            return this._super(...arguments);
+        },
+    });
+});
+
+
+/*odoo.define('theme_gifting.cart_count', function (require) {
+    "use strict";
+
+    const publicWidget = require('web.public.widget');
+
+    publicWidget.registry.GiftingCartCount = publicWidget.Widget.extend({
+        selector: '.cart-icon',
+
+        start: function () {
+            fetch('/shop/cart/update_json', {
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -28,4 +54,4 @@ odoo.define('theme_gifting.cart_count', function (require) {
             return this._super(...arguments);
         },
     });
-});
+});*/
