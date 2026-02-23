22b5d3207ae68a55905cf32d7bcefdad9de63d56
doubleprince-code <adesojiade1@gmail.com>
2026-02-23 14:45:01 -0800

    Best seller page update with controller v5

--- addons/gift_product_configurator/controllers/bestsellers.py
+++ addons/gift_product_configurator/controllers/bestsellers.py
@@ -1,18 +1,30 @@
 from odoo import http
 from odoo.http import request
 
-class BestSellerController(http.Controller):
+
+class BestSellersPage(http.Controller):
 
     @http.route(['/bestsellers'], type='http', auth='public', website=True)
     def bestsellers(self, **kwargs):
 
-        category = request.env['product.public.category'].sudo().search([
-            ('name', '=', 'Best Selling Promotional Products')
-        ], limit=1)
+        # Get category by name
+        category = request.env['product.public.category'].sudo().search(
+            [('name', '=', 'Best Selling Promotional Products')],
+            limit=1
+        )
+
+        if not category:
+            products = request.env['product.template']
+        else:
+            products = request.env['product.template'].sudo().search([
+                ('is_published', '=', True),
+                ('public_categ_ids', 'in', category.id),
+            ])
 
         return request.render(
-            'gift_product_configurator.bestsellers_page',
+            'gift_product_configurator.bestsellers_page_template',
             {
+                'products': products,
                 'category': category,
             }
         )
