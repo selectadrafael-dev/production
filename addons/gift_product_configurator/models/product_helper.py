56537f8b7c9061e2072a7cb137459b157690bbf8
doubleprince-code <adesojiade1@gmail.com>
2026-02-25 09:38:28 -0800

    Hpme sections revamps v23

--- addons/gift_product_configurator/models/product_helper.py
+++ addons/gift_product_configurator/models/product_helper.py
@@ -1,10 +1,12 @@
 from odoo import models
 
+
 class WebsiteProductHelper(models.AbstractModel):
     _name = 'gifting.product.helper'
     _description = 'Gifting Product Helper'
 
-    def get_latest_products(self, category_xml_id, limit=7, order_field='create_date'):
+    def get_latest_products(self, category_xml_id, limit=7):
+
         category = self.env.ref(category_xml_id, raise_if_not_found=False)
 
         if not category:
@@ -13,8 +15,9 @@ class WebsiteProductHelper(models.AbstractModel):
         return self.env['product.template'].search(
             [
                 ('public_categ_ids', 'in', category.id),
-                ('website_published', '=', True),
+                ('website_published', '=', True),   # ⭐ REQUIRED
+                ('sale_ok', '=', True),             # ⭐ optional but recommended
             ],
-            order=f'{order_field} desc',
+            order='create_date desc',
             limit=limit
         )
