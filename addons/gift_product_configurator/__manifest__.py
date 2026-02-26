dca4163ef13a214e58075e9eafc115a3367a0672
doubleprince-code <adesojiade1@gmail.com>
2026-02-25 19:04:25 -0800

    Hpme sections revamps v35

--- addons/gift_product_configurator/__manifest__.py
+++ addons/gift_product_configurator/__manifest__.py
@@ -11,6 +11,7 @@
 "data": [
     "views/product_page.xml",
     'views/bestsellers_page.xml',
+    'views/shop_category_hero.xml',
     #"views/product_components.xml",
     'data/sequence.xml',
     #'security/ir.model.access.csv',
@@ -19,7 +20,8 @@
     "web.assets_frontend": [
         "gift_product_configurator/static/src/scss/product_page.scss",
         "gift_product_configurator/static/src/scss/bestsellers.scss",
-        "gift_product_configurator/static/src/scss/quote_drawer.scss",
+        #"gift_product_configurator/static/src/scss/quote_drawer.scss",
+        "gift_product_configurator/static/src/scss/shop_hero.scss",
         "gift_product_configurator/static/src/js/configurator.js",
         "gift_product_configurator/static/src/js/quote_drawer.js"
     ],
