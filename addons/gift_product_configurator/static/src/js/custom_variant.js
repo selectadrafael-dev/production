dbac57cd189af0edfa45b1285ba45e98890c444b
doubleprince-code <adesojiade1@gmail.com>
2026-03-03 13:31:30 -0800

    variants vendor data too started v21

--- addons/gift_product_configurator/static/src/js/custom_variant.js
+++ addons/gift_product_configurator/static/src/js/custom_variant.js
@@ -1,38 +1,35 @@
 /** @odoo-module **/
 import publicWidget from "@web/legacy/js/public/public_widget";
-import { rpc } from "@web/core/network/rpc_service";
 
 publicWidget.registry.GiftVariantUpdate = publicWidget.Widget.extend({
-    selector: '#gift_product_detail',
-    events: {
-        'change .js_gift_variant_change': '_onVariantChange',
+    selector: '.oe_website_sale',
+    
+    start: function () {
+        // Force-disable zoom data to stop the calculation script
+        const $carousel = this.$('#o-carousel-product');
+        if ($carousel.length) {
+            $carousel.data('zoom', 0);
+            $carousel.attr('data-zoom', '0');
+        }
+        return this._super.apply(this, arguments);
     },
 
-    _onVariantChange: async function (ev) {
+    events: {
+        'change .js_variant_change': '_onVariantChange',
+    },
+
+    _onVariantChange: function (ev) {
         const $input = $(ev.currentTarget);
         const $block = $input.closest('.config-block');
         
-        // 1. Update UI Labels
-        $block.find('.selected-value').text($input.data('value_name'));
+        // 1. Update text label (e.g., Color: Red)
+        const valName = $input.data('value_name');
+        if (valName) {
+            $block.find('.selected-value').text(valName);
+        }
+
+        // 2. Visual active state for buttons
         $block.find('.variant-btn').removeClass('active');
         $input.closest('.variant-btn').addClass('active');
-
-        // 2. Collect selected values
-        const combination = this.$('.js_gift_variant_change:checked').map((i, el) => parseInt($(el).val())).get();
-        const productTemplateId = parseInt(this.$('.o_gift_variant_form').data('product-template-id'));
-
-        // 3. Fetch data from Odoo
-        const data = await rpc('/website_sale/get_combination_info', {
-            product_template_id: productTemplateId,
-            combination: combination,
-        });
-
-        // 4. Update Image and Price
-        if (data.product_id) {
-            this.$('#gift_main_image').attr('src', `/web/image/product.product/${data.product_id}/image_1024`);
-        }
-        if (data.price) {
-            this.$('#gift_price_container').html(data.price);
-        }
     },
 });
