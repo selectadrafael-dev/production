85e71d4828fd5a1cbc81f5a8006915c27ae69a6c
doubleprince-code <adesojiade1@gmail.com>
2026-03-03 14:42:20 -0800

    product page review v2

--- addons/gift_product_configurator/static/src/js/product_page.js
+++ addons/gift_product_configurator/static/src/js/product_page.js
@@ -5,22 +5,10 @@ publicWidget.registry.GiftProductRestructure = publicWidget.Widget.extend({
     selector: '#product_detail',
     
     start: function () {
-        // Move native components into the custom containers
-        this.$('#o-carousel-product').appendTo(this.$('#gift_gallery_placeholder'));
-        this.$('.js_add_cart_variants').appendTo(this.$('#gift_variant_form_placeholder'));
+        // Relocate Odoo's functional parts into your custom layout
+        this.$('#o-carousel-product').appendTo(this.$('#gift_gallery_hook'));
+        this.$('.js_add_cart_variants').appendTo(this.$('#gift_variant_hook'));
         
         return this._super.apply(this, arguments);
-    },
-
-    events: {
-        'change .js_variant_change': '_onUpdateHeader',
-    },
-
-    _onUpdateHeader: function (ev) {
-        // Updates the "Attribute: Value" label in your custom headers
-        const $input = $(ev.currentTarget);
-        const $block = $input.closest('.variant_attribute');
-        const valName = $input.parent().find('span').text();
-        $block.find('.selected-value').text(valName);
-    },
+    }
 });
