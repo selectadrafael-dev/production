9d423974ef0e587f32b809bb0587defd57e668dd
doubleprince-code <adesojiade1@gmail.com>
2026-03-03 13:10:30 -0800

    variants vendor data too started v17

--- addons/gift_product_configurator/static/src/js/custom_variant.js
+++ addons/gift_product_configurator/static/src/js/custom_variant.js
@@ -1,33 +1,38 @@
 /** @odoo-module **/
 import publicWidget from "@web/legacy/js/public/public_widget";
+import { jsonrpc } from "@web/core/network/rpc_service";
 
 publicWidget.registry.GiftVariantUpdate = publicWidget.Widget.extend({
-    selector: '.oe_website_sale',
-    
-    start: function () {
-        // Kill the zoom feature for this page to prevent the 'dataset' crash
-        if (this.$('#o-carousel-product').length) {
-            this.$('#o-carousel-product').data('zoom', 0);
-        }
-        return this._super.apply(this, arguments);
-    },
-
+    selector: '#custom_product_detail', // Target our new custom ID
     events: {
-        'change .js_variant_change': '_onVariantChange',
+        'change .js_custom_variant_change': '_onVariantChange',
     },
 
     _onVariantChange: function (ev) {
         const $input = $(ev.currentTarget);
-        const $block = $input.closest('.config-block');
+        const $parent = this.$el;
         
-        // Update Label Text
-        const valName = $input.data('value_name');
-        if (valName) {
-            $block.find('.selected-value').text(valName);
-        }
+        // 1. UI Updates (Text/Labels)
+        $input.closest('.config-block').find('.selected-value').text($input.data('value_name'));
+        $input.closest('.config-block').find('.variant-btn').removeClass('active');
+        $input.closest('label').addClass('active');
 
-        // Update Active Class
-        $block.find('.variant-btn').removeClass('active');
-        $input.closest('.variant-btn').addClass('active');
+        // 2. Fetch Variant Data (Price/Image) via RPC
+        const productTemplateId = parseInt($('.o_custom_variant_form').data('product-template-id'));
+        const variantValues = this.$('.js_custom_variant_change:checked').map((i, el) => parseInt($(el).val())).get();
+
+        jsonrpc('/website_sale/get_combination_info', {
+            product_template_id: productTemplateId,
+            combination: variantValues,
+        }).then((data) => {
+            // Update Image
+            if (data.display_image) {
+                this.$('.js_variant_img_target').attr('src', '/web/image/product.product/' + data.product_id + '/image_1024');
+            }
+            // Update Price
+            if (data.price) {
+                this.$('.js_custom_price_wrapper').html(data.price);
+            }
+        });
     },
 });
