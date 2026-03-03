c6af4fab70404eacb330e25b8937de54f80e627e
doubleprince-code <adesojiade1@gmail.com>
2026-03-03 15:17:37 -0800

    product page review v3

--- addons/gift_product_configurator/static/src/js/product_page.js
+++ addons/gift_product_configurator/static/src/js/product_page.js
@@ -1,14 +1,29 @@
-/** @odoo-module **/
-import publicWidget from "@web/legacy/js/public/public_widget";
-
-publicWidget.registry.GiftProductRestructure = publicWidget.Widget.extend({
-    selector: '#product_detail',
+(function () {
+    'use strict';
     
-    start: function () {
-        // Relocate Odoo's functional parts into your custom layout
-        this.$('#o-carousel-product').appendTo(this.$('#gift_gallery_hook'));
-        this.$('.js_add_cart_variants').appendTo(this.$('#gift_variant_hook'));
+    var syncLayout = function () {
+        // Target native Odoo elements
+        var carousel = document.getElementById('o-carousel-product');
+        var variants = document.querySelector('.js_add_cart_variants');
         
-        return this._super.apply(this, arguments);
-    }
-});
+        // Target your custom hooks
+        var galleryHook = document.getElementById('gift_gallery_hook');
+        var variantHook = document.getElementById('gift_variant_hook');
+
+        // Relocate elements if they exist
+        if (carousel && galleryHook) {
+            galleryHook.appendChild(carousel);
+        }
+        if (variants && variantHook) {
+            variantHook.appendChild(variants);
+        }
+    };
+
+    // Run on load and after Odoo's internal AJAX updates
+    document.addEventListener('DOMContentLoaded', syncLayout);
+    
+    // Safety check for Odoo's variant change events
+    $(document).on('variant_info_full', function() {
+        syncLayout();
+    });
+})();
