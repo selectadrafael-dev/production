389e3db1099b59afc73d4628ee0fa5409f193e37
doubleprince-code <adesojiade1@gmail.com>
2026-03-03 18:03:59 -0800

    product page review v19

--- addons/gift_product_configurator/static/src/js/product_page.js
+++ addons/gift_product_configurator/static/src/js/product_page.js
@@ -1,13 +1,11 @@
 (function () {
     'use strict';
-
     var relocateElements = function () {
         var carousel = document.getElementById('o-carousel-product');
         var variants = document.querySelector('.js_add_cart_variants');
         var galleryHook = document.getElementById('gift_gallery_hook');
         var variantHook = document.getElementById('gift_variant_hook');
 
-        // ONLY move if they aren't already in the right spot (STOPS THE LOOP)
         if (carousel && galleryHook && carousel.parentElement !== galleryHook) {
             galleryHook.appendChild(carousel);
         }
@@ -16,15 +14,8 @@
         }
     };
 
-    // Run on load
     window.addEventListener('load', relocateElements);
-
-    // Watch for Odoo's AJAX updates (Variant Change)
-    // We observe the main container instead of the whole body to stay safe
-    var observer = new MutationObserver(function (mutations) {
-        relocateElements();
-    });
-
+    var observer = new MutationObserver(relocateElements);
     var target = document.getElementById('product_detail');
     if (target) {
         observer.observe(target, { childList: true, subtree: true });
