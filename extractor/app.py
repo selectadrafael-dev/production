371043d526169f1a106256fdbbde416e7c2a1d47
doubleprince-code <adesojiade1@gmail.com>
2026-03-23 10:36:17 -0800

    Render extractor updated

--- extractor/app.py
+++ extractor/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import fitz  # PyMuPDF
 import base64
 import logging
+import os
 
 app = Flask(__name__)
 
@@ -86,4 +87,7 @@ def extract():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
