5dfc694707f878cf3352021ef375846c54f81bb3
doubleprince-code <adesojiade1@gmail.com>
2026-04-20 01:35:26 -0800

    Auto run cron v29

--- addons/gift_product_configurator/models/vendor_import_job.py
+++ addons/gift_product_configurator/models/vendor_import_job.py
@@ -1713,7 +1713,7 @@ class VendorImportJob(models.Model):
 
         self.env.cr.commit()
 
-    #==========create pdf and excel product======================
+    #==========create pdf and excel product==========================
 
     def create_products_pdf_excel(self):
 
