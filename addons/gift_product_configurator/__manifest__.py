f13df0b2ca8f051c4d6a3af5a44ba94ac8635ae1
doubleprince-code <adesojiade1@gmail.com>
2026-05-24 11:09:53 -0800

    Import page model restored v21

--- addons/gift_product_configurator/__manifest__.py
+++ addons/gift_product_configurator/__manifest__.py
@@ -30,7 +30,7 @@
     'views/product_template_views.xml',
     'views/product_purge_action.xml',
     'views/vendor_menu_security.xml',
-    'views/vendor_product_menu.xml',
+    #'views/vendor_product_menu.xml',
     #'views/vendor_body_class.xml',
 ],
 "assets": {
