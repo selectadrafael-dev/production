5aed61c014f91b14182aeccec0129d03d8bfff62
doubleprince-code <adesojiade1@gmail.com>
2026-05-23 22:21:04 -0800

    Import page model restored v10

---
+++ addons/gift_product_configurator/static/src/js/vendor_user_menu_hide.js
@@ -0,0 +1,24 @@
+odoo.define('gift_product_configurator.vendor_user_menu_hide', function (require) {
+    'use strict';
+
+    const session = require('web.session');
+    const registry = require('@web/core/registry');
+
+    const userMenuRegistry = registry.registry.category('user_menuitems');
+
+    session.user_has_group(
+        'gift_product_configurator.group_product_vendor'
+    ).then(function (isVendor) {
+
+        if (!isVendor) {
+            return;
+        }
+
+        userMenuRegistry.remove('documentation');
+        userMenuRegistry.remove('support');
+        userMenuRegistry.remove('odoo_account');
+        userMenuRegistry.remove('onboarding');
+
+    });
+
+});
