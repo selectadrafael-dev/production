22dc13acf38f9570bdfeb4d51852371fd0037a20
doubleprince-code <adesojiade1@gmail.com>
2026-05-25 15:10:26 -0800

    Delete stock revamp v2

--- addons/gift_product_configurator/models/__init__.py
+++ addons/gift_product_configurator/models/__init__.py
@@ -4,5 +4,5 @@ from . import product_public_category
 from . import custom_gifting_order
 from . import vendor_import_job
 from . import vendor_import_page
-from . import product_template
+#from . import product_template
 from . import product_delete_override
