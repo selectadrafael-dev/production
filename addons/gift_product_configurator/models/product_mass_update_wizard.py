1b3bb6bdc5c0e6aa026e3f17b9c9c608c9700c1a
doubleprince-code <adesojiade1@gmail.com>
2026-06-19 19:32:41 -0800

    Segement filtering v42

--- addons/gift_product_configurator/models/product_mass_update_wizard.py
+++ addons/gift_product_configurator/models/product_mass_update_wizard.py
@@ -1,6 +1,7 @@
 from odoo import models, fields
 
-class ProductMassUpdateWizard(models.TransientModel):
+class ProductMassUpdateWizard(models.Model):
+
     _name = "product.mass.update.wizard"
     _description = "Product Mass Update Wizard"
 
