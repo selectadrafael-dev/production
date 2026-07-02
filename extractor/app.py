f35921fc78cd91b3d3a4c1befb955a83d596fc64
doubleprince-code <adesojiade1@gmail.com>
2026-07-02 07:47:34 -0800

    Extractor v2 update test run v2

--- extractor/app.py
+++ extractor/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger(__name__)
 
-# ================= HOME =================
+# ================= HOME ====================
 @app.route("/")
 def home():
     # return "OK"
